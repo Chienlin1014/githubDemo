62486877b31c445fa82b6647b2547f651e4a8fd2
2301021 <cl.chia@iisigroup.com>
2024-03-19 16:34:10 +0800

    init

--- src/main/java/org/example/githubdemo/rest/HelloWord.java
+++ src/main/java/org/example/githubdemo/rest/HelloWord.java
@@ -1,2 +1,14 @@
-package org.example.githubdemo.rest;public class HelloWord {
+package org.example.githubdemo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWord {
+    @RequestMapping("/")
+    public String hello() {
+
+        return "Hello World!";
+
+    }
 }
